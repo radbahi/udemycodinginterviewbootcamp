4d2fcfc466b1c4ab19c956e1fe1a96eb0c3cb451
Radouane Bahi <radbahi@gmail.com>
2020-06-13 12:47:45 -0400

    tried levelWidth, used instructor

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,41 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+  // instructor solution
+  const arr = [root, "s"];
+  const counters = [0];
+  while (arr.length > 1) {
+    const node = arr.shift();
+    if (node === "s") {
+      counters.push(0);
+      arr.push("s");
+    } else {
+      arr.push(...node.children);
+      counters[counters.length - 1]++;
+    }
+  }
+  return counters;
+}
+// function levelWidth(root) { // my function, but it didn't work. got a FATAL ERROR: invalid array length Allocation failed - JavaScript heap out of memory
+
+//     let counter = 0
+//     const widthArr = []
+//     const tempArr = [root, 's']
+//     while (tempArr.length > 1) {
+//         if (tempArr[0] = 's') {
+//             tempArr.shift()
+//             tempArr.push('s')
+//             widthArr.push(counter)
+//             counter = 0
+//         } else {
+//             let node = tempArr[0]
+//             tempArr.push(node.children)
+//             counter++
+//             tempArr.shift()
+//         }
+//     }
+//     return widthArr
+// }
 
 module.exports = levelWidth;
