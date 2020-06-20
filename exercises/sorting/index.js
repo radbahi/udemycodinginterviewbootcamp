7910e94604648e707eb76a4f4eb22e3f6473686d
Radouane Bahi <radbahi@gmail.com>
2020-06-20 11:21:43 -0400

    bubble sort complete

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -1,9 +1,30 @@
 // --- Directions
 // Implement bubbleSort, selectionSort, and mergeSort
 
-function bubbleSort(arr) {
-
+function bubbleSort(arr) { // instructor solution. had to change only a few things
+    for (let j = 0; j < arr.length; j++) {
+        for (let i = 0; i < (arr.length - j - 1); i++) {
+            if (arr[i] > arr[i + 1]) {
+                const tempHolder = arr[i]
+                arr[i] = arr[i + 1]
+                arr[i + 1] = tempHolder
+            }
+        }
+    }
+    return arr
 }
+
+// function bubbleSort(arr) { // my failed solution
+//     for (let lengthNumber of arr) {
+//         for (let i = 0; i < arr.length - 1; i++) {
+//             if (arr[i] > arr[i + 1]) {
+//                 const tempHolder = arr[i]
+//                 arr[i] = arr[i + 1]
+//                 arr[i + 1] = tempHolder
+//             }
+//         }
+//     }
+// }
 
 function selectionSort(arr) {
 
